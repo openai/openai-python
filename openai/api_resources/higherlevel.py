8ef54f5069c68113bda1e1536dcdaf7f64d86839
hallacy <hallacy@openai.com>
2021-02-16 15:54:24 -0800

    Make higherlevel have class methods so you can call with openai.HigherLevel.answer (#7)
    * refactor retriever endpoint
    
    * Actually just make everything a classmethod so you can call it like openai.HigherLevel

--- openai/api_resources/higherlevel.py
+++ openai/api_resources/higherlevel.py
@@ -4,14 +4,21 @@ from openai.api_resources.abstract.engine_api_resource import EngineAPIResource
 class HigherLevel(EngineAPIResource):
     api_prefix = "higherlevel"
 
+    @classmethod
     def get_url(self, base):
         return "/%s/%s" % (self.api_prefix, base)
 
-    def classification(self, **params):
-        return self.request("post", self.get_url("classifications"), params)
+    @classmethod
+    def classification(cls, **params):
+        instance = cls()
+        return instance.request("post", cls.get_url("classifications"), params)
 
-    def answer(self, **params):
-        return self.request("post", self.get_url("answers"), params)
+    @classmethod
+    def answer(cls, **params):
+        instance = cls()
+        return instance.request("post", cls.get_url("answers"), params)
 
-    def file_set_search(self, **params):
-        return self.request("post", self.get_url("file_set_search"), params)
+    @classmethod
+    def file_set_search(cls, **params):
+        instance = cls()
+        return instance.request("post", cls.get_url("file_set_search"), params)
