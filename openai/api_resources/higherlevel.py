10e995bbddddfccca43c431b4994d6855f406744
hallacy <hallacy@openai.com>
2021-02-16 14:53:48 -0800

    refactor retriever endpoint (#6)

--- openai/api_resources/higherlevel.py
+++ openai/api_resources/higherlevel.py
@@ -13,5 +13,5 @@ class HigherLevel(EngineAPIResource):
     def answer(self, **params):
         return self.request("post", self.get_url("answers"), params)
 
-    def retriever_file_set_search(self, **params):
-        return self.request("post", self.get_url("retriever_file_set_search"), params)
+    def file_set_search(self, **params):
+        return self.request("post", self.get_url("file_set_search"), params)
