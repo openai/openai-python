2b264c39f8ce6960e81df0cd401c7581767a0f64
Sorin Suciu <sorin.suciu@microsoft.com>
2022-03-11 11:19:57 -0800

    Add support for fine-tunning and files using the Azure API. (#76)
    * Add support for fine-tunning and files using the Azure API.
    
    * Small changes + version bumps
    
    * Version bump after merge
    
    * fix typo
    
    * adressed comments

--- openai/api_resources/abstract/api_resource.py
+++ openai/api_resources/abstract/api_resource.py
@@ -8,7 +8,8 @@ from openai.util import ApiType
 
 class APIResource(OpenAIObject):
     api_prefix = ""
-    azure_api_prefix = 'openai/deployments'
+    azure_api_prefix = 'openai'
+    azure_deployments_prefix = 'deployments'
 
     @classmethod
     def retrieve(cls, id, api_key=None, request_id=None, **params):
@@ -46,20 +47,21 @@ class APIResource(OpenAIObject):
                 "id",
             )
         api_version = self.api_version or openai.api_version
+        extn = quote_plus(id)
 
         if self.typed_api_type == ApiType.AZURE:
             if not api_version:
                 raise error.InvalidRequestError("An API version is required for the Azure API type.")
+            
             if not operation:
-                raise error.InvalidRequestError(
-                    "The request needs an operation (eg: 'search') for the Azure OpenAI API type."
-                )
-            extn = quote_plus(id)
-            return "/%s/%s/%s?api-version=%s" % (self.azure_api_prefix, extn, operation, api_version)
+                base = self.class_url()
+                return "/%s%s/%s?api-version=%s" % (self.azure_api_prefix, base, extn, api_version)
+
+            return "/%s/%s/%s/%s?api-version=%s" % (
+                self.azure_api_prefix, self.azure_deployments_prefix, extn, operation, api_version)
 
         elif self.typed_api_type == ApiType.OPEN_AI:
             base = self.class_url()
-            extn = quote_plus(id)
             return "%s/%s" % (base, extn)
 
         else:
@@ -75,6 +77,7 @@ class APIResource(OpenAIObject):
         url_,
         api_key=None,
         api_base=None,
+        api_type=None,
         request_id=None,
         api_version=None,
         organization=None,
@@ -85,6 +88,7 @@ class APIResource(OpenAIObject):
             api_version=api_version,
             organization=organization,
             api_base=api_base,
+            api_type=api_type
         )
         response, _, api_key = requestor.request(
             method_, url_, params, request_id=request_id
@@ -92,3 +96,10 @@ class APIResource(OpenAIObject):
         return util.convert_to_openai_object(
             response, api_key, api_version, organization
         )
+
+    @classmethod
+    def _get_api_type_and_version(cls, api_type: str, api_version: str):
+        typed_api_type = ApiType.from_str(api_type) if api_type else ApiType.from_str(openai.api_type)
+        typed_api_version = api_version or openai.api_version
+        return (typed_api_type, typed_api_version)
+
