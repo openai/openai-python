d486548a5e677c65d78086bafd9bc89311a1f7dc
Zafer Çavdar <zafercavdar@yahoo.com>
2022-09-18 11:14:30 +0300

    Reverted auto-formatted linting

--- openai/api_resources/moderation.py
+++ openai/api_resources/moderation.py
@@ -4,20 +4,18 @@ from openai.openai_object import OpenAIObject
 
 
 class Moderation(OpenAIObject):
-    VALID_MODEL_NAMES: List[str] = [
-        "text-moderation-stable", "text-moderation-latest"]
+    VALID_MODEL_NAMES: List[str] = ["text-moderation-stable", "text-moderation-latest"]
 
     @classmethod
     def get_url(self):
         return "/moderations"
 
     @classmethod
-    def create(cls, input: Union[str, List[str]], model: Optional[str] = None,
-               api_key: Optional[str] = None):
+    def create(cls, input: Union[str, List[str]], model: Optional[str] = None, api_key: Optional[str] = None):
         if model is not None and model not in cls.VALID_MODEL_NAMES:
             raise ValueError(
                 f"The parameter model should be chosen from {cls.VALID_MODEL_NAMES} "
-                f"and it is default to be None.",
+                f"and it is default to be None."
             )
 
         instance = cls(api_key=api_key)
