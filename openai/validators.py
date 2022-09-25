6f3554487272658bd728687298d96c5f054f683e
Serina Grill <serinagrill@gmail.com>
2022-09-25 13:43:16 -0700

    Initial commit

--- openai/validators.py
+++ openai/validators.py
@@ -128,7 +128,7 @@ def duplicated_rows_validator(df, fields=["prompt", "completion"]):
     This validator will suggest to the user to remove duplicate rows if they exist.
     """
     duplicated_rows = df.duplicated(subset=fields)
-    duplicated_indexes = df.reset_index().index[duplicated_rows].tolist()
+    duplicated_indexes = df.index[duplicated_rows].tolist()
     immediate_msg = None
     optional_msg = None
     optional_fn = None
@@ -158,17 +158,26 @@ def long_examples_validator(df):
 
     ft_type = infer_task_type(df)
     if ft_type != "open-ended generation":
-        long_examples = df.apply(
-            lambda x: len(x.prompt) + len(x.completion) > 10000, axis=1
-        )
-        long_indexes = df.reset_index().index[long_examples].tolist()
+        def find_long_indexes(d):
+            long_examples = d.apply(
+                lambda x: len(x.prompt) + len(x.completion) > 10000, axis=1
+            )
+            long_indexes = d.index[long_examples].tolist()
+            return long_indexes
+
+        long_indexes = find_long_indexes(df)
 
         if len(long_indexes) > 0:
             immediate_msg = f"\n- There are {len(long_indexes)} examples that are very long. These are rows: {long_indexes}\nFor conditional generation, and for classification the examples shouldn't be longer than 2048 tokens."
             optional_msg = f"Remove {len(long_indexes)} long examples"
 
             def optional_fn(x):
-                return x.drop(long_indexes)
+                
+                long_indexes_to_drop = find_long_indexes(x)
+                if long_indexes != long_indexes_to_drop:
+                    print(
+                        f"The indices of the long examples has changed as a result of a previously applied recommendation.\nThe {len(long_indexes_to_drop)} long examples to be dropped are now at the following indices: {long_indexes_to_drop}")
+                return x.drop(long_indexes_to_drop)
 
     return Remediation(
         name="long_examples",
