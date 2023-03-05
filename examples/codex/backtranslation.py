ea3ea9aa749f2b589a352e80d24783e1dc110adf
ethanknights <ethanknights@hotmail.co.uk>
2023-03-05 15:32:08 +0000

    make SQL example pointer consistent

--- examples/codex/backtranslation.py
+++ examples/codex/backtranslation.py
@@ -1,1 +1,2 @@
-# this example has moved to https://github.com/openai/openai-cookbook/blob/main/examples/Backtranslation_of_SQL_queries.py
+# This code example has moved. You can now find it in the [OpenAI Cookbook](https://github.com/openai/openai-cookbook)
+# at [examples/Backtranslation_of_SQL_queries](https://github.com/openai/openai-cookbook/blob/main/examples/Backtranslation_of_SQL_queries.py)
