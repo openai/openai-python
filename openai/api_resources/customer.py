9d6573b47fddee6792bd50644dac4928bc6eb902
MuleHakim <mulukenhakim97@gmail.com>
2023-04-04 10:56:17 +0300

    Refactor Customer class to use variable for URL generation and improve code clarity

--- openai/api_resources/customer.py
+++ openai/api_resources/customer.py
@@ -4,14 +4,17 @@ from openai.openai_object import OpenAIObject
 class Customer(OpenAIObject):
     @classmethod
     def get_url(cls, customer, endpoint):
-        return f"/customer/{customer}/{endpoint}"
+        url = f"/customer/{customer}/{endpoint}"
+        return url
 
     @classmethod
     def create(cls, customer, endpoint, **params):
+        url = cls.get_url(customer, endpoint)
         instance = cls()
-        return instance.request("post", cls.get_url(customer, endpoint), params)
+        return instance.request("post", url, params)
 
     @classmethod
     def acreate(cls, customer, endpoint, **params):
+        url = cls.get_url(customer, endpoint)
         instance = cls()
-        return instance.arequest("post", cls.get_url(customer, endpoint), params)
+        return instance.arequest("post", url, params)
