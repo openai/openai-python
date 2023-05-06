03f848e3c91e1cb6e1a7860ea91e219f82cc97f4
Krista Pratico <krpratic@microsoft.com>
2023-05-06 09:26:23 -0700

    allow api_version to be set by environment variable (#438)

--- openai/__init__.py
+++ openai/__init__.py
@@ -48,8 +48,9 @@ api_key_path: Optional[str] = os.environ.get("OPENAI_API_KEY_PATH")
 organization = os.environ.get("OPENAI_ORGANIZATION")
 api_base = os.environ.get("OPENAI_API_BASE", "https://api.openai.com/v1")
 api_type = os.environ.get("OPENAI_API_TYPE", "open_ai")
-api_version = (
-    "2023-03-15-preview" if api_type in ("azure", "azure_ad", "azuread") else None
+api_version = os.environ.get(
+    "OPENAI_API_VERSION",
+    ("2023-03-15-preview" if api_type in ("azure", "azure_ad", "azuread") else None),
 )
 verify_ssl_certs = True  # No effect. Certificates are always verified.
 proxy = None
