c6073bf6182ab9b4515ab5b020ea370e2ea267d8
ansumanswain <142146902+ansumanswain@users.noreply.github.com>
2023-10-17 23:23:30 +0530

    Update model.py
    Added context length attribute to the model objects returned on the list and retrieve models endpoint. #587

--- openai/api_resources/model.py
+++ openai/api_resources/model.py
@@ -1,5 +1,9 @@
 from openai.api_resources.abstract import DeletableAPIResource, ListableAPIResource
-
-
+{
+  "context_limit": {
+    "type": "integer",
+    "description": "The maximum context length that the model can effectively handle."
+  },
+}
 class Model(ListableAPIResource, DeletableAPIResource):
     OBJECT_NAME = "models"
