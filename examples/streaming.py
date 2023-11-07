8c55fecc470457eb9b754b32c32d528650a253e7
franz101 <fkrekeler@gmail.com>
2023-11-06 16:04:21 -0800

    more upgrades

--- examples/streaming.py
+++ examples/streaming.py
@@ -33,7 +33,7 @@ def sync_main() -> None:
 async def async_main() -> None:
     client = AsyncOpenAI()
     response = await client.completions.create(
-        model="text-davinci-002",
+        model="gpt-3.5-turbo-instruct",
         prompt="1,2,3,",
         max_tokens=5,
         temperature=0,
