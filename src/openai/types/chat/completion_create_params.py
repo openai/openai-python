8865093a56d5d21a4d4e3a2bb4032f3165e8383e
lukesamkharadze <lukesamkharadze@icloud.com>
2023-11-27 14:38:13 -0800

    change transform model type

--- src/openai/types/chat/completion_create_params.py
+++ src/openai/types/chat/completion_create_params.py
@@ -36,19 +36,29 @@ class CompletionCreateParamsBase(TypedDict, total=False):
         Union[
             str,
             Literal[
-                "gpt-4-1106-preview",
-                "gpt-4-vision-preview",
-                "gpt-4",
-                "gpt-4-0314",
-                "gpt-4-0613",
-                "gpt-4-32k",
-                "gpt-4-32k-0314",
-                "gpt-4-32k-0613",
-                "gpt-3.5-turbo",
-                "gpt-3.5-turbo-16k",
-                "gpt-3.5-turbo-0301",
-                "gpt-3.5-turbo-0613",
-                "gpt-3.5-turbo-16k-0613",
+                "claude-v1"
+                "claude-v2"
+                "claude-instant-v1"
+                "llama-2-70b-chat"
+                "gpt-3.5-turbo"
+                "gpt-4-32k"
+                "gpt-4"
+                "gpt-4-turbo-128k"
+            ],
+            List[
+                Union[
+                    str,
+                    Literal[
+                        "claude-v1"
+                        "claude-v2"
+                        "claude-instant-v1"
+                        "llama-2-70b-chat"
+                        "gpt-3.5-turbo"
+                        "gpt-4-32k"
+                        "gpt-4"
+                        "gpt-4-turbo-128k"
+                    ],
+                ]
             ],
         ]
     ]
