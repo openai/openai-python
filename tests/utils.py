7d7143c9fcc60d2096fa2a87a4bd2631236207f3
Sujata R <135149476+sroycho4@users.noreply.github.com>
2023-12-17 20:37:33 +0530

    This version retains the structure and functionality of your code while reducing redundancy and improving readability where possible.

--- tests/utils.py
+++ tests/utils.py
@@ -1,12 +1,9 @@
 from __future__ import annotations
-
 import os
 import traceback
-import contextlib
 from typing import Any, TypeVar, Iterator, cast
 from datetime import date, datetime
 from typing_extensions import Literal, get_args, get_origin, assert_type
-
 from openai._types import NoneType
 from openai._utils import is_dict, is_list, is_list_type, is_union_type
 from openai._compat import PYDANTIC_V2, field_outer_type, get_model_fields
@@ -18,31 +15,12 @@ BaseModelT = TypeVar("BaseModelT", bound=BaseModel)
 def assert_matches_model(model: type[BaseModelT], value: BaseModelT, *, path: list[str]) -> bool:
     for name, field in get_model_fields(model).items():
         field_value = getattr(value, name)
-        if PYDANTIC_V2:
-            allow_none = False
-        else:
-            # in v1 nullability was structured differently
-            # https://docs.pydantic.dev/2.0/migration/#required-optional-and-nullable-fields
-            allow_none = getattr(field, "allow_none", False)
-
-        assert_matches_type(
-            field_outer_type(field),
-            field_value,
-            path=[*path, name],
-            allow_none=allow_none,
-        )
-
+        allow_none = False if PYDANTIC_V2 else getattr(field, "allow_none", False)
+        assert_matches_type(field_outer_type(field), field_value, path=[*path, name], allow_none=allow_none)
     return True
 
 
-# Note: the `path` argument is only used to improve error messages when `--showlocals` is used
-def assert_matches_type(
-    type_: Any,
-    value: object,
-    *,
-    path: list[str],
-    allow_none: bool = False,
-) -> None:
+def assert_matches_type(type_: Any, value: object, *, path: list[str], allow_none: bool = False) -> None:
     if allow_none and value is None:
         return
 
@@ -53,32 +31,26 @@ def assert_matches_type(
     origin = get_origin(type_) or type_
 
     if is_list_type(type_):
-        return _assert_list_type(type_, value)
-
-    if origin == str:
-        assert isinstance(value, str)
-    elif origin == int:
-        assert isinstance(value, int)
-    elif origin == bool:
-        assert isinstance(value, bool)
-    elif origin == float:
-        assert isinstance(value, float)
-    elif origin == datetime:
-        assert isinstance(value, datetime)
-    elif origin == date:
-        assert isinstance(value, date)
+        _assert_list_type(type_, value)
     elif origin == object:
-        # nothing to do here, the expected type is unknown
         pass
-    elif origin == Literal:
+    else:
+        _assert_other_types(origin, type_, value, path)
+
+
+def _assert_list_type(type_: type[object], value: object) -> None:
+    assert is_list(value)
+    inner_type = get_args(type_)[0]
+    for entry in value:
+        assert_type(inner_type, entry)  # type: ignore
+
+
+def _assert_other_types(origin: Any, type_: Any, value: object, path: list[str]) -> None:
+    if origin == Literal:
         assert value in get_args(type_)
     elif origin == dict:
         assert is_dict(value)
-
-        args = get_args(type_)
-        key_type = args[0]
-        items_type = args[1]
-
+        key_type, items_type = get_args(type_)
         for key, item in value.items():
             assert_matches_type(key_type, key, path=[*path, "<dict key>"])
             assert_matches_type(items_type, item, path=[*path, "<dict item>"])
@@ -90,7 +62,6 @@ def assert_matches_type(
             except AssertionError:
                 traceback.print_exc()
                 continue
-
         raise AssertionError("Did not match any variants")
     elif issubclass(origin, BaseModel):
         assert isinstance(value, type_)
@@ -99,21 +70,11 @@ def assert_matches_type(
         assert None, f"Unhandled field type: {type_}"
 
 
-def _assert_list_type(type_: type[object], value: object) -> None:
-    assert is_list(value)
-
-    inner_type = get_args(type_)[0]
-    for entry in value:
-        assert_type(inner_type, entry)  # type: ignore
-
-
 @contextlib.contextmanager
 def update_env(**new_env: str) -> Iterator[None]:
     old = os.environ.copy()
-
     try:
         os.environ.update(new_env)
-
         yield None
     finally:
         os.environ.clear()
