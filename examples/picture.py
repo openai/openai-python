323afe1be94634329845329eb0d8087e3475e304
Logan Kilpatrick <23kilpatrick23@gmail.com>
2024-01-03 10:52:08 -0600

    Update picture.py

--- examples/picture.py
+++ examples/picture.py
@@ -1,16 +1,19 @@
 #!/usr/bin/env python
 
-from pathlib import Path
-
 from openai import OpenAI
 
 # gets OPENAI_API_KEY from your environment variables
 openai = OpenAI()
+
 prompt = "An astronaut lounging in a tropical resort in space, pixel art"
+model = "dall-e-3"
+
 def main() -> None:
-    # Generate the image based on the prompt
-    response = openai.images.generate(prompt=prompt)
-    # Prints response with url link to image
+    # Generate an image based on the prompt
+    response = openai.images.generate(prompt=prompt, model=model)
+    
+    # Prints response containing a URL link to image
     print(response)
+    
 if __name__ == "__main__":
     main()
