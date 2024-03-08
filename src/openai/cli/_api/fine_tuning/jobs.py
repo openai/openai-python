d44dd66d0bb684628586d2850f4c299c7ac8fd80
ammar <ahmedammarnaseer@gmail.com>
2024-03-08 15:46:09 +0000

    add fine_tuning.jobs.list

--- src/openai/cli/_api/fine_tuning/jobs.py
+++ src/openai/cli/_api/fine_tuning/jobs.py
@@ -4,7 +4,9 @@ from typing import TYPE_CHECKING
 from argparse import ArgumentParser
 
 from ..._utils import get_client, print_model
+from ...._types import NOT_GIVEN, NotGivenOr
 from ..._models import BaseModel
+from ....pagination import SyncCursorPage
 from ....types.fine_tuning import (
     FineTuningJob,
 )
@@ -28,12 +30,30 @@ def register(subparser: _SubParsersAction[ArgumentParser]) -> None:
     )
 
     sub = subparser.add_parser("fine_tuning.jobs.list")
+    sub.add_argument(
+        "-a",
+        "--after",
+        help="Identifier for the last job from the previous pagination request. If provided, only jobs created after this job will be returned.",
+    )
+    sub.add_argument(
+        "-l",
+        "--limit",
+        help="Number of fine-tuning jobs to retrieve.",
+        type=int,
+    )
+    sub.set_defaults(func=CLIFineTuningJobs.list, args_model=CLIFineTuningJobsListArgs)
+
     sub = subparser.add_parser("fine_tuning.jobs.cancel")
     sub = subparser.add_parser("fine_tuning.jobs.list_events")
 
 
 class CLIFineTuningJobsRetrieveArgs(BaseModel):
     id: str
+
+class CLIFineTuningJobsListArgs(BaseModel):
+    after: NotGivenOr[str] = NOT_GIVEN
+    limit: NotGivenOr[int] = NOT_GIVEN
+
 
 class CLIFineTuningJobs:
     @staticmethod
@@ -42,3 +62,12 @@ class CLIFineTuningJobs:
             fine_tuning_job_id=args.id
         )
         print_model(fine_tuning_job)
+    
+    @staticmethod
+    def list(args: CLIFineTuningJobsListArgs) -> None:
+        fine_tuning_jobs: SyncCursorPage[
+            FineTuningJob
+        ] = get_client().fine_tuning.jobs.list(
+            after=args.after or NOT_GIVEN, limit=args.limit or NOT_GIVEN
+        )
+        print_model(fine_tuning_jobs)
