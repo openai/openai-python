62346f1246a7fa357445a9ee64876e8a99bf0507
ExplorerGT92 <John.S@eloquent-algorithmics.com>
2024-08-20 15:43:23 +0000

    Change maxium to maximum

--- src/openai/types/beta/threads/run.py
+++ src/openai/types/beta/threads/run.py
@@ -138,7 +138,7 @@ class Run(BaseModel):
 
     This can be useful for storing additional information about the object in a
     structured format. Keys can be a maximum of 64 characters long and values can be
-    a maxium of 512 characters long.
+    a maximum of 512 characters long.
     """
 
     model: str
