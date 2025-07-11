81951fc08592fe415f3a30c27d29e434c45bc034
Johan Stenberg <johan.stenberg@microsoft.com>
2025-07-11 16:06:21 -0700

    Add refresh auth headers (sync and async) as alternate approach to allow bearer tokens to be updated

--- src/openai/lib/azure.py
+++ src/openai/lib/azure.py
@@ -614,7 +614,7 @@ class AsyncAzureOpenAI(BaseAzureClient[httpx.AsyncClient, AsyncStream[Any]], Asy
             "api-version": self._api_version,
             "deployment": self._azure_deployment or model,
         }
-        if self.api_key != "<missing API key>":
+        if self.api_key and self.api_key != "<missing API key>":
             auth_headers = {"api-key": self.api_key}
         else:
             token = await self._get_azure_ad_token()
