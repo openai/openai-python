3f4ae725af53e631ddc128c1c6862ecf0b08e073
Robert Craigie <robert@craigie.dev>
2025-08-08 12:28:58 +0100

    fix(types): revert ChatCompletionToolParam to a TypedDict

---
+++ src/openai/types/chat/chat_completion_tool_param.py
@@ -0,0 +1,11 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from __future__ import annotations
+
+from typing_extensions import TypeAlias
+
+from .chat_completion_function_tool_param import ChatCompletionFunctionToolParam
+
+__all__ = ["ChatCompletionToolParam"]
+
+ChatCompletionToolParam: TypeAlias = ChatCompletionFunctionToolParam
