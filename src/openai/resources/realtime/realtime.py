25d16be18bcd11e00a853e8f4af881c76098e0d0
Johan Stenberg (MSFT) <johan.stenberg@microsoft.com>
2025-09-03 08:52:05 -0700

    feat(client): support callable api_key (#2588)
    Co-authored-by: Krista Pratico <krpratic@microsoft.com>

--- src/openai/resources/realtime/realtime.py
+++ src/openai/resources/realtime/realtime.py
@@ -326,6 +326,7 @@ class AsyncRealtimeConnectionManager:
             raise OpenAIError("You need to install `openai[realtime]` to use this method") from exc
 
         extra_query = self.__extra_query
+        await self.__client._refresh_api_key()
         auth_headers = self.__client.auth_headers
         if is_async_azure_client(self.__client):
             url, auth_headers = await self.__client._configure_realtime(self.__model, extra_query)
@@ -507,6 +508,7 @@ class RealtimeConnectionManager:
             raise OpenAIError("You need to install `openai[realtime]` to use this method") from exc
 
         extra_query = self.__extra_query
+        self.__client._refresh_api_key()
         auth_headers = self.__client.auth_headers
         if is_azure_client(self.__client):
             url, auth_headers = self.__client._configure_realtime(self.__model, extra_query)
