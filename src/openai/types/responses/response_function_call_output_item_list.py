8ac532dd933c1e70a2213b06d504edecd33b25cc
Sudhanwa <sudhanwamd@gmail.com>
2026-06-21 19:07:33 +0530

    fix(responses): resolve issue #3075

--- src/openai/types/responses/response_function_call_output_item_list.py
+++ src/openai/types/responses/response_function_call_output_item_list.py
@@ -1,10 +1,10 @@
 # File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
 
-from typing import List
+from typing import List, Union
 from typing_extensions import TypeAlias
 
-from .response_function_call_output_item import ResponseFunctionCallOutputItem
+from .response_function_call_output_item import ResponseFunctionCallOutputItem, Compaction
 
 __all__ = ["ResponseFunctionCallOutputItemList"]
 
-ResponseFunctionCallOutputItemList: TypeAlias = List[ResponseFunctionCallOutputItem]
+ResponseFunctionCallOutputItemList: TypeAlias = List[Union[ResponseFunctionCallOutputItem, Compaction]]
