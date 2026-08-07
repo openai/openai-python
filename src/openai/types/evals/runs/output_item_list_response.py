b0f95b511d7642a674203432c58eed57bc42aeb9
apcha-oai <228803254+apcha-oai@users.noreply.github.com>
2026-08-06 19:54:33 -0700

    chore(api): Update generated-file header attribution to Castiron
    Castiron-Internal-PR: https://github.com/openai/openai-python-internal/pull/17
    Castiron-Source-SHA: c1af0d75f9b09c7f1a6d5ad24964f41bded74ff3
    Castiron-Public-Base-SHA: 0c09a3fe815184f0a46fbf18b1aba84a467c854e

--- src/openai/types/evals/runs/output_item_list_response.py
+++ src/openai/types/evals/runs/output_item_list_response.py
@@ -1,4 +1,4 @@
-# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+# File generated from our OpenAPI spec by Castiron. See CONTRIBUTING.md for details.
 
 from typing import TYPE_CHECKING, Dict, List, Optional
 from typing_extensions import Literal
