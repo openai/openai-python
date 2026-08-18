870037e821de0ee2554d5464aa7df093f7636d84
apcha-oai <228803254+apcha-oai@users.noreply.github.com>
2026-08-18 16:44:02 -0400

    chore: remove unused dependencies and pin build tooling
    Castiron-Internal-PR: https://github.com/openai/openai-python-internal/pull/38
    Castiron-Source-SHA: e03f48da399389414ae129d320d9e2102e16ec9b
    Castiron-Public-Base-SHA: 44c1560ad2f707d68e3f28d66aae602f7626e187

--- tests/test_files.py
+++ tests/test_files.py
@@ -2,7 +2,6 @@ from pathlib import Path
 
 import anyio
 import pytest
-from dirty_equals import IsDict, IsList, IsBytes, IsTuple
 
 from openai._files import to_httpx_files, deepcopy_with_paths, async_to_httpx_files
 from openai._utils import extract_files
@@ -12,35 +11,30 @@ readme_path = Path(__file__).parent.parent.joinpath("README.md")
 
 def test_pathlib_includes_file_name() -> None:
     result = to_httpx_files({"file": readme_path})
-    print(result)
-    assert result == IsDict({"file": IsTuple("README.md", IsBytes())})
+    assert result == {"file": ("README.md", readme_path.read_bytes())}
 
 
 def test_tuple_input() -> None:
     result = to_httpx_files([("file", readme_path)])
-    print(result)
-    assert result == IsList(IsTuple("file", IsTuple("README.md", IsBytes())))
+    assert result == [("file", ("README.md", readme_path.read_bytes()))]
 
 
 @pytest.mark.asyncio
 async def test_async_pathlib_includes_file_name() -> None:
     result = await async_to_httpx_files({"file": readme_path})
-    print(result)
-    assert result == IsDict({"file": IsTuple("README.md", IsBytes())})
+    assert result == {"file": ("README.md", readme_path.read_bytes())}
 
 
 @pytest.mark.asyncio
 async def test_async_supports_anyio_path() -> None:
     result = await async_to_httpx_files({"file": anyio.Path(readme_path)})
-    print(result)
-    assert result == IsDict({"file": IsTuple("README.md", IsBytes())})
+    assert result == {"file": ("README.md", readme_path.read_bytes())}
 
 
 @pytest.mark.asyncio
 async def test_async_tuple_input() -> None:
     result = await async_to_httpx_files([("file", readme_path)])
-    print(result)
-    assert result == IsList(IsTuple("file", IsTuple("README.md", IsBytes())))
+    assert result == [("file", ("README.md", readme_path.read_bytes()))]
 
 
 def test_string_not_allowed() -> None:
